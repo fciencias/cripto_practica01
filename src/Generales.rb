6ba36558780c60268dd9a3bd32c4af0885d3b2c4
Jose Cruz Galindo <jose.galindo@ciencias.unam.mx>
2012-10-01 02:56:11 -0500

    Ya lo pinta bonito solo falta checar lo de los n-gramas

--- src/Generales.rb
+++ src/Generales.rb
@@ -7,7 +7,7 @@ require './Manejador'
 class Generales
   
   manejador = nil
-  TAMANO = 25
+  TAMANO = 26
   @texto = nil
 
   ##
@@ -18,35 +18,61 @@ class Generales
     @texto = manejador.contenido
   end
 
-  def frecuencias
-    tabla = {"A"=>0,"B"=>1,"C"=>2,"D"=>3,"E"=>4,"F"=>5,"G"=>6,"H"=>7,"I"=>8,
-      "J"=>9,"K"=>10,"L"=>11,"M"=>12,"N"=>13,"O"=>14,"P"=>15,"Q"=>16,"R"=>17,
-      "S"=>18,"T"=>19,"U"=>20,"V"=>21,"W"=>22,"X"=>23,"Y"=>24,"Z"=>25}
-    @texto.each_char { |c|
+  ## Dado un texto cifrado nos permite contar el numero de veces
+  # que se repite cada palabra en el texto, recibe como parametro
+  # la indicacion true para que imprima la lista de frecuencias 
+  # ordenadas
+  def frecuencias(_imprime)
+    tabla = {"A"=>0,"B"=>0,"C"=>0,"D"=>0,"E"=>0,"F"=>0,"G"=>0,"H"=>0,"I"=>0,
+      "J"=>0,"K"=>0,"L"=>0,"M"=>0,"N"=>0,"O"=>0,"P"=>0,"Q"=>0,"R"=>0,
+      "S"=>0,"T"=>0,"U"=>0,"V"=>0,"W"=>0,"X"=>0,"Y"=>0,"Z"=>0}
+    @texto.each_char {|c|
       valor = tabla[c]
       if valor != nil
         tabla[c] = valor.succ
-      end }
-      ordenados = selection_sort(tabla)
-      ordenados.each {|ele| print ele[0], " -> ", ele[1], ", "}
+      end}
+      sorted = selection_sort(tabla)
+      if _imprime
+        sorted.each {|ele| 
+          puts ele[0] + " -- " + ele[1].to_s
+        }
+      end
+      return sorted
   end
 
+  ##
+  #
+  def n_gramas(indice)
+    tamano_texto = @texto.length
+    if indice > 7 || indice < 2
+      puts "Solo puedo darte de 2 a 7 -gramas"
+      return
+    end
+    arreglo = frecuencias(false)
+    10.times do |i|
+      frecuen = arreglo[i]
+      letra = frecuen[0]
+      puts letra
+    end
+    
+  end
 
   ## Ordena una tabla has usando el algorito de selection sort, que
   # aunque no es el mejor al ser pocos casos no es tan grave y es muy 
   # facil de implementar.
   def selection_sort(tabla_hash)
     ordenados = tabla_hash.to_a 
-    for i in 0...TAMANO-2
-      min = i
-      for j in i+1...TAMANO-1
-        if (ordenados[j])[1] >= (ordenados[min])[1]
-          min = j
+    for i in 0..TAMANO-2
+      max = i
+      for j in i+1..TAMANO-1
+        if (ordenados[j])[1] > (ordenados[max])[1]
+          max = j
         end
-        aux = ordenados[i]
-        ordenados[i] = ordenados[min]
-        ordenados[min] = aux
-        # swap y asignacion
+        if (ordenados[i])[1] < (ordenados[max])[1]
+          aux = ordenados[i]
+          ordenados[i] = ordenados[max]
+          ordenados[max] = aux
+        end
       end
     end
     return ordenados
@@ -61,4 +87,5 @@ class Generales
 end #class
 
 grl = Generales.new("vigenere_completo.txt")
-grl.frecuencias
+#grl.frecuencias
+grl.n_gramas(7)
